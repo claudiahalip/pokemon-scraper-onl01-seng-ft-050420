aac849dce857eda659cffeffdc0d25828a0a28dc
claudia v <claudia4ro@yahoo.com>
2020-06-05 03:28:01 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,15 +18,15 @@ class Pokemon
     # @id = DB[:conn].execute("SELECT last_insert_rowid()FROM pokemon")[0][0]
   end 
   
-  def self.find(id, db)
-    sql = <<-SQL
-      SELECT * FROM pokemon
-      WHERE id = ?
-      SQL
-    DB.execute(sql, [id]).flatten
-    Pokemon.new(id: id, name: DB.execute(sql, id).flatten[1], type: DB.execute(sql, id).flatten[2], db: db)
+  # def self.find(id, db)
+  #   sql = <<-SQL
+  #     SELECT * FROM pokemon
+  #     WHERE id = ?
+  #     SQL
+  #   DB.execute(sql, [id]).flatten
+  #   Pokemon.new(id: id, name: DB.execute(sql, id).flatten[1], type: DB.execute(sql, id).flatten[2], db: db)
     
         
-  end
+  # end
   
 end
